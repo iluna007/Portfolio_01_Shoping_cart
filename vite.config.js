fec2df5675aafac9eb3ab0491cc6e752617b3d2c
Iker Luna <146373472+iluna007@users.noreply.github.com>
2026-02-01 21:26:45 -0600

    vite conf

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/Portfolio_01_Shopping_cart/',
+  base: '/Portfolio_01_Shoping_cart/',
   plugins: [react()],
 })
