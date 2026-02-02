00fec598ed5b16530077842fa59330d5897660c0
Iker Luna <146373472+iluna007@users.noreply.github.com>
2026-02-01 21:22:36 -0600

    actualizar

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio_01_Shopping_cart/',
   plugins: [react()],
 })
